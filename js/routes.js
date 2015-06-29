cc54736e52eb939576fb55bcc31f625906e975f2
atriatech94 <hamid_7555@hotmail.com>
2015-06-29 20:36:37 +0430

    n2

--- js/routes.js
+++ js/routes.js
@@ -8,14 +8,7 @@ angular.module('pooshak')
             templateUrl: 'pages/posts/index-2.html',
             controller: 'PostController',
         })
-        .when('/post_detail', {
-            templateUrl: 'pages/post_detail/index.html',
-            controller: 'PostdetailController',         
-        })
-        .when('/myprofile', {
-            templateUrl: 'pages/myprofile/index.html',
-            controller: 'MyprofileController',
-        })
+     
 		.when('/', {
             templateUrl: 'pages/home/index.html',
             controller: 'IndexController',
