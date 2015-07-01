753eed50501a34e11145d02c90775aa645f76578
atriatech94 <hamid_7555@hotmail.com>
2015-07-01 12:45:46 +0430

    11
    11

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 angular.module('pooshak')
     .config(function($routeProvider) {
         $routeProvider.when('/home', {
-            templateUrl: '/pages/home/index-44.html',
+            templateUrl: 'pages/home/index-44.html',
             controller: 'IndextwoController',
         })
         .when('/post/:cat', {
@@ -10,7 +10,7 @@ angular.module('pooshak')
         })
      
 		.when('/', {
-            templateUrl: '/pages/home/index-44.html',
+            templateUrl: 'pages/home/index-44.html',
             controller: 'IndextwoController',
 		})
 		.otherwise({ redirectTo: '/' });
